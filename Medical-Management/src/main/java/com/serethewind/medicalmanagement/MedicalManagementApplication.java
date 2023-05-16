66eae081f6352a0fc2daeb3034cbe3af67f4818e
serethewind <osasereu@gmail.com>
2023-05-16 16:48:39 +0100

    initial commit

---
+++ Medical-Management/src/main/java/com/serethewind/medicalmanagement/MedicalManagementApplication.java
@@ -0,0 +1,13 @@
+package com.serethewind.medicalmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalManagementApplication.class, args);
+	}
+
+}
